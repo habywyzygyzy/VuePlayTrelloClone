d3e905f252bab601a3e230b7b6ce2c4dade15fbd
habywyzygyzy <202119@edu.p.lodz.pl>
2019-02-03 15:08:24 +0100

    Application (homecontroller) fix

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,9 +5,8 @@ import play.mvc.Result;
 import views.html.index;
 
 public class Application extends Controller {
-
     public static Result index() {
         String userID = session().getOrDefault("user.id", "null");
-        return ok(index.render("You're logged in!" + userID));
+        return ok(views.html.index.render("You're logged in!" + userID));
     }
 }
